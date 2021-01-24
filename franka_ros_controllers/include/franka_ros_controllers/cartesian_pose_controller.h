932e6b8f38c25896316f62f5b9d96dcd2c2c8dfb
rachelholladay <rachel.holladay@gmail.com>
2021-01-24 13:48:59 -0500

    Ugly implementation of cartesian pose

--- franka_ros_controllers/include/franka_ros_controllers/cartesian_pose_controller.h
+++ franka_ros_controllers/include/franka_ros_controllers/cartesian_pose_controller.h
@@ -5,6 +5,7 @@
 #include <array>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include <controller_interface/multi_interface_controller.h>
 #include <franka_hw/franka_state_interface.h>
@@ -14,13 +15,17 @@
 
 #include <franka_hw/franka_cartesian_command_interface.h>
 
+#include <geometry_msgs/PoseStamped.h>
+#include <Eigen/Dense>
+#include <franka_hw/franka_model_interface.h>
+
 namespace franka_ros_controllers {
 
 class CartesianPoseController
     : public controller_interface::MultiInterfaceController<franka_hw::FrankaPoseCartesianInterface,
                                                             franka_hw::FrankaStateInterface> {
  public:
-  bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle) override;
   void starting(const ros::Time&) override;
   void update(const ros::Time&, const ros::Duration& period) override;
 
@@ -29,6 +34,19 @@ class CartesianPoseController
   std::unique_ptr<franka_hw::FrankaCartesianPoseHandle> cartesian_pose_handle_;
   ros::Duration elapsed_time_;
   std::array<double, 16> initial_pose_{};
+
+  std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
+  std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
+
+  double filter_params_{0.005};
+  Eigen::Vector3d position_d_;
+  Eigen::Quaterniond orientation_d_;
+  Eigen::Vector3d position_d_target_;
+  Eigen::Quaterniond orientation_d_target_;
+
+  // Equilibrium pose subscriber
+  ros::Subscriber sub_equilibrium_pose_;
+  void equilibriumPoseCallback(const geometry_msgs::PoseStampedConstPtr& msg);
 };
 
 }  // namespace franka_ros_controllers
--- franka_ros_controllers/src/cartesian_pose_controller.cpp
+++ franka_ros_controllers/src/cartesian_pose_controller.cpp
@@ -13,11 +13,19 @@
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
 
+#include <franka/robot_state.h>
+#include "pseudo_inversion.h"
+
 namespace franka_ros_controllers {
 
-bool CartesianPoseController::init(hardware_interface::RobotHW* robot_hardware,
+bool CartesianPoseController::init(hardware_interface::RobotHW* robot_hw,
                                           ros::NodeHandle& node_handle) {
-  cartesian_pose_interface_ = robot_hardware->get<franka_hw::FrankaPoseCartesianInterface>();
+
+  sub_equilibrium_pose_ = node_handle.subscribe(
+      "/equilibrium_pose", 20, &CartesianPoseController::equilibriumPoseCallback, this,
+      ros::TransportHints().reliable().tcpNoDelay());
+
+  cartesian_pose_interface_ = robot_hw->get<franka_hw::FrankaPoseCartesianInterface>();
   if (cartesian_pose_interface_ == nullptr) {
     ROS_ERROR(
         "CartesianPoseController: Could not get Cartesian Pose "
@@ -31,6 +39,30 @@ bool CartesianPoseController::init(hardware_interface::RobotHW* robot_hardware,
     return false;
   }
 
+  std::vector<std::string> joint_names;
+  if (!node_handle.getParam("joint_names", joint_names) || joint_names.size() != 7) {
+    ROS_ERROR(
+        "CartesianPoseController: Invalid or no joint_names parameters provided, "
+        "aborting controller init!");
+    return false;
+  }
+
+  auto* model_interface = robot_hw->get<franka_hw::FrankaModelInterface>();
+  if (model_interface == nullptr) {
+    ROS_ERROR_STREAM(
+        "CartesianPoseController: Error getting model interface from hardware");
+    return false;
+  }
+  try {
+    model_handle_ = std::make_unique<franka_hw::FrankaModelHandle>(
+        model_interface->getHandle(arm_id + "_model"));
+  } catch (hardware_interface::HardwareInterfaceException& ex) {
+    ROS_ERROR_STREAM(
+        "CartesianPoseController: Exception getting model handle from interface: "
+        << ex.what());
+    return false;
+  }
+
   try {
     cartesian_pose_handle_ = std::make_unique<franka_hw::FrankaCartesianPoseHandle>(
         cartesian_pose_interface_->getHandle(arm_id + "_robot"));
@@ -40,42 +72,36 @@ bool CartesianPoseController::init(hardware_interface::RobotHW* robot_hardware,
     return false;
   }
 
-  auto state_interface = robot_hardware->get<franka_hw::FrankaStateInterface>();
+  auto state_interface = robot_hw->get<franka_hw::FrankaStateInterface>();
   if (state_interface == nullptr) {
     ROS_ERROR("CartesianPoseController: Could not get state interface from hardware");
     return false;
   }
 
-  try {
-    auto state_handle = state_interface->getHandle(arm_id + "_robot");
-
-    std::array<double, 7> q_start{{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-    for (size_t i = 0; i < q_start.size(); i++) {
-      if (std::abs(state_handle.getRobotState().q_d[i] - q_start[i]) > 0.1) {
-        ROS_ERROR_STREAM(
-            "CartesianPoseController: Robot is not in the expected starting position for "
-            "running this example. Run `roslaunch franka_example_controllers move_to_start.launch "
-            "robot_ip:=<robot-ip> load_gripper:=<has-attached-gripper>` first.");
-        return false;
-      }
-    }
-  } catch (const hardware_interface::HardwareInterfaceException& e) {
-    ROS_ERROR_STREAM(
-        "CartesianPoseController: Exception getting state handle: " << e.what());
-    return false;
-  }
+  position_d_.setZero();
+  orientation_d_.coeffs() << 0.0, 0.0, 0.0, 1.0;
+  position_d_target_.setZero();
+  orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
 
   return true;
 }
 
 void CartesianPoseController::starting(const ros::Time& /* time */) {
   initial_pose_ = cartesian_pose_handle_->getRobotState().O_T_EE_d;
-  elapsed_time_ = ros::Duration(0.0);
+
+  franka::RobotState initial_state = state_handle_->getRobotState();
+  Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
+
+  // set equilibrium point to current state
+  position_d_ = initial_transform.translation();
+  orientation_d_ = Eigen::Quaterniond(initial_transform.linear());
+  position_d_target_ = initial_transform.translation();
+  orientation_d_target_ = Eigen::Quaterniond(initial_transform.linear());
 }
 
 void CartesianPoseController::update(const ros::Time& /* time */,
                                      const ros::Duration& period) {
-  elapsed_time_ += period;
+  /*elapsed_time_ += period;
 
   double radius = 0.3;
   double angle = M_PI / 4 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec()));
@@ -84,7 +110,46 @@ void CartesianPoseController::update(const ros::Time& /* time */,
   std::array<double, 16> new_pose = initial_pose_;
   new_pose[12] -= delta_x;
   new_pose[14] -= delta_z;
+  cartesian_pose_handle_->setCommand(new_pose);*/
+
+  // Take desired position, quaternion and create homogenous transform to send
+  //TODO there has a to be a better way
+  std::array<double, 16> new_pose;
+  std::copy_n(position_d_.data(), 3, new_pose.begin()+12);
+  /*new_pose[12] = position_d_[0];
+  new_pose[13] = position_d_[1];
+  new_pose[14] = position_d_[2];*/
+  Eigen::Matrix3d rotation_d_ = orientation_d_.normalized().toRotationMatrix();
+  new_pose[0] = rotation_d_(0, 0);
+  new_pose[1] = rotation_d_(0, 1);
+  new_pose[2] = rotation_d_(0, 2);
+  new_pose[4] = rotation_d_(1, 0);
+  new_pose[5] = rotation_d_(1, 1);
+  new_pose[6] = rotation_d_(1, 2);
+  new_pose[8] = rotation_d_(2, 0);
+  new_pose[9] = rotation_d_(2, 1);
+  new_pose[10] = rotation_d_(2, 2);
   cartesian_pose_handle_->setCommand(new_pose);
+
+  // Update parameters via filtering
+  position_d_ = filter_params_ * position_d_target_ + (1.0 - filter_params_) * position_d_;
+  Eigen::AngleAxisd aa_orientation_d(orientation_d_);
+  Eigen::AngleAxisd aa_orientation_d_target(orientation_d_target_);
+  aa_orientation_d.axis() = filter_params_ * aa_orientation_d_target.axis() + (1.0 - filter_params_) * aa_orientation_d.axis();
+  aa_orientation_d.angle() = filter_params_ * aa_orientation_d_target.angle() + (1.0 - filter_params_) * aa_orientation_d.angle();
+  orientation_d_ = Eigen::Quaterniond(aa_orientation_d);
+
+}
+
+void CartesianPoseController::equilibriumPoseCallback(
+    const geometry_msgs::PoseStampedConstPtr& msg) {
+  position_d_target_ << msg->pose.position.x, msg->pose.position.y, msg->pose.position.z;
+  Eigen::Quaterniond last_orientation_d_target(orientation_d_target_);
+  orientation_d_target_.coeffs() << msg->pose.orientation.x, msg->pose.orientation.y,
+      msg->pose.orientation.z, msg->pose.orientation.w;
+  if (last_orientation_d_target.coeffs().dot(orientation_d_target_.coeffs()) < 0.0) {
+    orientation_d_target_.coeffs() << -orientation_d_target_.coeffs();
+  }
 }
 
 }  // namespace franka_ros_controllers
