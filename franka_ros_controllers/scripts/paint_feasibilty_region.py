2d3d9562c5cb4198df9fe8a7c5ba1482ae8b757f
neeld <nddoshi9@gmail.com>
2021-06-02 16:47:58 -0400

    added code for painting feasbility region

--- franka_ros_controllers/scripts/paint_feasibilty_region.py
+++ franka_ros_controllers/scripts/paint_feasibilty_region.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     # object parameters
     obj_params = dict()
     obj_params['pivot'] = np.array([0., 0.])
-    obj_params['mgl'] = .7
-    obj_params['theta0'] = np.pi/12
-    obj_params['mu_contact'] = 0.2
-    obj_params['mu_ground'] = 1.0
+    obj_params['mgl'] = 0.2
+    obj_params['theta0'] = 0.
+    obj_params['mu_contact'] = 0.15
+    obj_params['mu_ground'] = 0.3
     obj_params['l_contact'] = 0.065
 
     # position control parameters
@@ -28,36 +28,136 @@ if __name__ == "__main__":
     Nmax = 40  # maximum force
     dnom = -0.11  # nominal value of d
 
-    NPTS = 20
-    theta_vec, s_vec = np.linspace(-np.pi / 4, np.pi / 4,
+    NPTS = 30
+    theta_vec, s_vec = np.linspace(-np.pi / 3, np.pi / 3,
                                    NPTS), np.linspace(-0.04, 0.04, NPTS)
     theta_grid, s_grid = np.meshgrid(theta_vec, s_vec)
-    is_feasible = np.zeros([NPTS, NPTS, 3])
+    is_feasible = np.zeros([NPTS, NPTS, 5])
 
     for i in range(NPTS):
         for j in range(NPTS):
-            for k in range(3):
-                print(s_grid[i, j], (180/np.pi) * theta_grid[i,j])
+            for k in range(5):
+                # print(s_grid[i, j], (180/np.pi) * theta_grid[i,j])
                 # update current pose
                 current_pose = np.array([dnom, s_grid[i, j], theta_grid[i, j]])
                 pbal_impedance_inv.contact_pose_target = current_pose
 
                 # solve for wrench
-                robot_wrench = pbal_impedance_inv.solve_linear_program_mode(Nmax,
-                                                                            mode=k-1)
-                
+                robot_wrench = pbal_impedance_inv.solve_linear_program_mode(
+                    Nmax, mode=k - 1)
+
                 if robot_wrench is not None:
                     is_feasible[i, j, k] = 1
-    
 
-    fig, axs = plt.subplots(1,3)
-    titles = ['Sticking', 'Sliding Plus', 'Sliding Minus']
+    fig, axs = plt.subplots(1, 5)
+    titles = [
+        'Sticking', 'Robot sliding right', 'Robot sliding left',
+        'Pivot sliding left', 'Pivot sliding right'
+    ]
     for mode, ax in enumerate(axs):
         is_feasible_mode = is_feasible[:, :, mode]
-        ax.scatter((180/np.pi) * theta_grid[is_feasible_mode == 1], s_grid[is_feasible_mode == 1], color='g')
-        ax.scatter((180/np.pi) * theta_grid[is_feasible_mode == 0], s_grid[is_feasible_mode == 0], color='r')
-        ax.set_xlabel('Theta [deg]')
-        ax.set_ylabel('S [m]')
+        ax.scatter(s_grid[is_feasible_mode == 1],
+                   (180 / np.pi) * theta_grid[is_feasible_mode == 1],
+                   color='g')
+        ax.scatter(s_grid[is_feasible_mode == 0],
+                   (180 / np.pi) * theta_grid[is_feasible_mode == 0],
+                   color='r')
+
+        if mode == 0:
+
+            # one boundary
+            theta_boundary1 = theta_vec[theta_vec > obj_params['theta0']]
+            s_pivot_boundary1 = -dnom * np.tan(theta_boundary1 - np.arctan(
+                obj_params['mu_ground'])) - obj_params['l_contact'] / 2
+            ax.plot(s_pivot_boundary1, (180 / np.pi) * theta_boundary1, 'k')
+
+            # other boundary
+            theta_boundary2 = theta_vec[theta_vec < obj_params['theta0']]
+            s_pivot_boundary2 = -dnom * np.tan(theta_boundary2 + np.arctan(
+                obj_params['mu_ground'])) + obj_params['l_contact'] / 2
+            ax.plot(s_pivot_boundary2, (180 / np.pi) * theta_boundary2, 'k')
+
+            ax.vlines(
+                -obj_params['mu_contact'] * dnom + obj_params['l_contact'] / 2,
+                -(180 / np.pi) * np.pi / 3, (180 / np.pi) * np.pi / 3)
+            ax.vlines(
+                obj_params['mu_contact'] * dnom - obj_params['l_contact'] / 2,
+                -(180 / np.pi) * np.pi / 3, (180 / np.pi) * np.pi / 3)
+
+            ax.hlines((180 / np.pi) * (np.arctan(obj_params['mu_contact']) +
+                                       np.arctan(obj_params['mu_ground'])),
+                      -0.04, 0.04)
+
+            ax.hlines(
+                -(180 / np.pi) * (np.arctan(obj_params['mu_contact']) +
+                                  np.arctan(obj_params['mu_ground'])), -0.04,
+                0.04)
+
+        if mode == 1:
+
+            ax.hlines(
+                -(180 / np.pi) * (np.arctan(obj_params['mu_contact']) -
+                                  np.arctan(obj_params['mu_ground'])), -0.04,
+                0.04)
+
+            ax.vlines(
+                obj_params['mu_contact'] * dnom + obj_params['l_contact'] / 2,
+                -(180 / np.pi) * np.pi / 3,
+                (180 / np.pi) * obj_params['theta0'])
+
+            # other boundary
+            theta_boundary2 = theta_vec[theta_vec < obj_params['theta0']]
+            s_pivot_boundary2 = -dnom * np.tan(theta_boundary2 + np.arctan(
+                obj_params['mu_ground'])) + obj_params['l_contact'] / 2
+            ax.plot(s_pivot_boundary2, (180 / np.pi) * theta_boundary2, 'k')
+
+        if mode == 2:
+
+            ax.hlines((180 / np.pi) * (np.arctan(obj_params['mu_contact']) -
+                                       np.arctan(obj_params['mu_ground'])),
+                      -0.04, 0.04)
+
+            ax.vlines(
+                -obj_params['mu_contact'] * dnom - obj_params['l_contact'] / 2,
+                (180 / np.pi) * obj_params['theta0'],
+                (180 / np.pi) * np.pi / 3)
+
+            # one boundary
+            theta_boundary1 = theta_vec[theta_vec > obj_params['theta0']]
+            s_pivot_boundary1 = -dnom * np.tan(theta_boundary1 - np.arctan(
+                obj_params['mu_ground'])) - obj_params['l_contact'] / 2
+            ax.plot(s_pivot_boundary1, (180 / np.pi) * theta_boundary1, 'k')
+
+        if mode == 3:
+
+            ax.hlines((180 / np.pi) * (np.arctan(obj_params['mu_contact']) -
+                                       np.arctan(obj_params['mu_ground'])),
+                      -0.04, 0.04)
+         
+            # one boundary
+            theta_boundary1 = theta_vec[theta_vec > obj_params['theta0']]
+            s_pivot_boundary1 = -dnom * np.tan(theta_boundary1 + np.arctan(
+                obj_params['mu_ground'])) - obj_params['l_contact'] / 2
+            ax.plot(s_pivot_boundary1, (180 / np.pi) * theta_boundary1, 'k')
+
+
+        if mode == 4:
+
+            ax.hlines(
+                -(180 / np.pi) * (np.arctan(obj_params['mu_contact']) -
+                                  np.arctan(obj_params['mu_ground'])), -0.04,
+                0.04)
+   
+            # other boundary
+            theta_boundary2 = theta_vec[theta_vec < obj_params['theta0']]
+            s_pivot_boundary2 = -dnom * np.tan(theta_boundary2 - np.arctan(
+                obj_params['mu_ground'])) + obj_params['l_contact'] / 2
+            ax.plot(s_pivot_boundary2, (180 / np.pi) * theta_boundary2, 'k')
+
+        ax.hlines((180 / np.pi) * obj_params['theta0'], -0.04, 0.04)
+        ax.set_xlabel('S [m]')
+        ax.set_ylabel('Theta [deg]')
+        ax.set_xlim([-0.04, 0.04])
         ax.set_title(titles[mode])
 
     plt.show()
